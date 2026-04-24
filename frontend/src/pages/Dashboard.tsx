e613952dd0840d62369cfd1f86e77e6937f9359e
Shubham Awasthi <shubhamawasthi021@gmail.com>
2026-04-24 16:49:07 +0530

    sidebar

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,19 +1,21 @@
 import Navbar from "../components/navbar";
+import SideBar from "../components/sidebar";
+
 
 export default function Dashboard(){
+   
+
+
+    
+     
     return (
         
         <div>
-            <Navbar/>
-            <div className="min-h-screen flex text-lg  ">
-                <aside className="bg-black py-10 px-4 w-80 hidden md:block">
-                    <nav className="">
-                    <button className="font-bold text-white rounded-xl bg-green-400 px-8 py-2 m-2">Dashboard</button>
-                    <button className="font-bold text-white rounded-xl bg-green-400 px-8 py-2 m-2">Dashboard</button>
-                    <button className="font-bold text-white rounded-xl bg-green-400 px-8 py-2 m-2">Dashboard</button>
-                    <button className="font-bold text-white rounded-xl bg-green-400 px-8 py-2 m-2">Dashboard</button>
-                    </nav>
-                </aside>
+            <div>
+                <Navbar/>
+            </div>
+            <div>
+                <SideBar/>
             </div>
         </div>
 
