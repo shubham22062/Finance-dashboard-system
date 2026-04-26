36c7d71759542b069fad7189590dad7d985be85a
Shubham Awasthi <shubhamawasthi021@gmail.com>
2026-04-26 22:49:19 +0530

    chnages

--- frontend/src/pages/Transactions.tsx
+++ frontend/src/pages/Transactions.tsx
@@ -8,6 +8,8 @@ export default function Transactions(){
     const [type,setType] = useState("Income");
     const [category, setCategory] = useState("")
 
+    const [isOpen, setIsOpen] = useState(false);
+
 
     return(
         
@@ -22,7 +24,8 @@ export default function Transactions(){
                     <p className="text-gray-400 text pt-1">Manage and track all Financial transactions</p>
                 </div>
                 <div>
-                    <button className="text-white rounded-xl font-bold bg-green-400 border border-green-400 p-2 w-40 transition-transform duration-300 hover:scale-105 hover:bg-green-500 ">Add Transction</button>
+                    <button onClick={()=>setIsOpen(true)}
+                     className="text-white rounded-xl font-bold bg-green-400 border border-green-400 p-2 w-40 transition-transform duration-300 hover:scale-105 hover:bg-green-500 ">Add Transction</button>
                 </div>
             </div>
 
@@ -64,12 +67,99 @@ export default function Transactions(){
                     </div>
                 </div>
 
+                
+
             </div>
           
+          <div className=" mt-5 border-2 w-full min-h-full border-gray-300 rounded-xl">
+
+
+            <div className="grid grid-cols-6 grid-flow-row-dense gap-4 p-2">
+                <span>Date</span>
+              <span>Time</span>
+              <span>category</span>
+             <span>Notes</span>
+             <span>Amout</span>
+             <span>Actions</span>
+             </div>
+            
+          </div>
 
         </div>
      </div>
-                
+      {isOpen && (
+  <div
+    className="fixed inset-0 bg-black/40 flex items-center justify-center z-50"
+    onClick={() => setIsOpen(false)} // close on outside click
+  >
+    
+    {/* Modal Box */}
+    <div
+      className="bg-white w-150 h-150 p-4 rounded-2xl shadow-xl relative"
+      onClick={(e) => e.stopPropagation()} // prevent close inside
+    >
+
+      {/* Close Button */}
+      <button
+        onClick={() => setIsOpen(false)}
+        className="absolute top-2 right-3 text-xl font-bold"
+      >
+        ✕
+      </button>
+
+      <h2 className="text-xl font-bold mb-4">Add Transaction</h2>
+
+      <form className="flex flex-col gap-3">
+
+        <label>Amount</label>
+        <input
+          type="number"
+          placeholder="Amount"
+          className="border p-2 rounded border-gray-300 bg-gray-200"
+        />
+
+        <label>Type</label>
+        <select className="border p-2 rounded border-gray-300 bg-gray-200">
+          <option>Income</option>
+          <option>Expense</option>
+        </select>
+
+        <label>Category</label>
+        <select className="border p-2 rounded border-gray-300 bg-gray-200">
+          <option>Select Category</option>
+          <option>Food</option>
+          <option>Travel</option>
+          <option>Shopping</option>
+          <option>Others</option>
+        </select>
+
+        <label>Date</label>
+        <input type="date" className="border p-2 rounded border-gray-300 bg-gray-200" />
+
+        <label>Note</label>
+        <textarea
+          placeholder="Note"
+          className="border p-2 rounded border-gray-300 bg-gray-200"
+        />
+        <div className="flex gap-4">
+            <button
+          type="submit"
+          className="bg-green-400 w-45 text-white p-2 rounded-md mt-2 hover:bg-green-500"
+        >
+          Create Transaction
+        </button>
+
+        <button className="rounded-md bg-gray-300 border-2 border-gray-400  w-40">
+            Cancel
+        </button>
+        </div>
+        
+
+      </form>
+    </div>
+  </div>
+)}       
      </div>
+     
     )
 }
