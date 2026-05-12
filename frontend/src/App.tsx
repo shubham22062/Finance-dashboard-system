b2b08110837fb05f448ced118924a5a43c59f068
Shubham Awasthi <shubhamawasthi021@gmail.com>
2026-05-12 13:33:32 +0530

    fixess

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Landing from "./pages/landing";
+import Landing from "./pages/Landing";
 import SignUp from "./pages/SignUp";
 import Dashboard from "./pages/Dashboard";
 import SignIn from "./pages/SignIn";
